6a60ca187730db1c3e34f5d88b4cf8f3040801fc
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-04 11:22:02 +0300

    1.7. Конструктор [#468425]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,37 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(){
+
+    }
+
+    public Error(boolean active, int status, String message){
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void showInfo(){
+        System.out.println();
+        System.out.println("Active : " + active);
+        System.out.println("Status : " + status);
+        System.out.println("Message : " + message);
+    }
+
+    public static void main(String[] args) {
+        Error err0 = new Error();
+        Error err1 = new Error(true, 32, "Err1");
+        Error err2 = new Error(false, 17, "Err2");
+        Error err3 = new Error(true, 1, "Err3");
+
+        err0.showInfo();
+        err1.showInfo();
+        err2.showInfo();
+        err3.showInfo();
+    }
+
+}
