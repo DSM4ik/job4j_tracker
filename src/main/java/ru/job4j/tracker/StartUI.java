6de392f509a0fbdbd2bcbea5082c2d2c8c81a4c4
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-11 16:51:47 +0300

    9. toString [#468446] - правка

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,20 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-
-        Tracker tracker = new Tracker();
-        Item item = new Item("Заявление 1");
-        tracker.add(item);
-        Item itemResult = tracker.findById(1);
-
-        System.out.println(itemResult.getId() + " - " + itemResult.getName());
-
-        /* Item item = new Item();
-        LocalDateTime itemDateTime = item.getCreated();
-        System.out.println(itemDateTime.format(
-                           DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss")
-                                              )
-                           );*/
-
+        Item item = new Item(1, "bug1");
+        System.out.println(item);
     }
 }
