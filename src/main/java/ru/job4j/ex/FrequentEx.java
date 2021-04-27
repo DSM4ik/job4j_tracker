fd94f2d747cae71d102d588d2ac3a66c55d8241c
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-27 23:15:43 +0300

    0. Что такое исключение. [#468395]

---
+++ src/main/java/ru/job4j/ex/FrequentEx.java
@@ -0,0 +1,10 @@
+package ru.job4j.ex;
+
+public class FrequentEx {
+    public static void main(String[] args) {
+        String[] shops = new String[]{"Ebay", "Amazon", "Ozon"};
+        for (int i = 0; i < shops.length; i++) {
+            System.out.println(shops[i]);
+        }
+    }
+}
