d73ff7dbe114c5625b003ebdc79d4fe604056672
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-28 23:56:12 +0300

    0.3. Тестирование исключений с junit [#468388]

---
+++ src/test/java/ru/job4j/ex/CountTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
+
+
+public class CountTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenStartGreaterThenFinish() {
+        Count.add(10, 2);
+    }
+}
---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.ex;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FactTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenInputParameterLess0() {
+        Fact fact = new Fact();
+        fact.calc(-3);
+    }
+}
