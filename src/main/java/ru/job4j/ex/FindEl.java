e2612d55812a83c9494b6a4838560214d0e847a8
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-29 21:41:02 +0300

    0.5. Иерархия исключений и множественный catch. [#468390]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,5 +1,12 @@
 package ru.job4j.ex;
 
+/**
+ *  метод indexOf - этот метод проверяет, что в списке есть ключ шаблона сообщения.
+ *  Метод может выкинуть исключение ElementNotFoundException.
+ *
+ * Метод sent отправляет сообщение, если ключ  входит в ключи запрещенных ключей.
+ * Метод может выкинуть исключение ElementAbuseException.
+ */
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
@@ -15,9 +22,22 @@ public class FindEl {
         return rsl;
     }
 
-    public static void main(String[] args) {
+    public static boolean sent(String value, String[] abuses) throws ElementAbuseException {
+        for (int i = 0; i < abuses.length; i++) {
+            if (abuses[i] == value) {
+                throw new ElementAbuseException("Key is Illegal");
+            }
+        }
+        return true;
+    }
+
+    public static void process(String[] values, String keys, String[] abuses, String value) {
         try {
-            int finded = indexOf(new String[]{"q", "w", "e"}, "z");
+            if (indexOf(values, keys) != -1) {
+                sent(value, abuses);
+            }
+        } catch (ElementAbuseException e) {
+            e.printStackTrace();
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
