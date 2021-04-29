645383feb2d568521ddeed4bf8853087d8e84d20
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-04-29 20:45:54 +0300

    0.4. Пользовательские исключения. [#468389]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Not find element in array");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int finded = indexOf(new String[]{"q", "w", "e"}, "z");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
