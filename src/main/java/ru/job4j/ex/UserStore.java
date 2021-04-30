0a9c448363a08e578e4a9dd10bb32ba712105cc0
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-01 00:08:38 +0300

    0.5. Иерархия исключений и множественный catch. [#468390] - правка

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -16,7 +16,7 @@ public class UserStore {
     }
 
     public static boolean validate(User user) throws UserInvalidException {
-        if (user.getUsername().length() < 3 || !user.isValid()) {
+        if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User is not valid");
         }
         return true;
