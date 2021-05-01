56a05873fb94cc8984405beed5b4178007ae9652
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-01 23:45:19 +0300

    2.1. Тест на ValidateInput [#468393]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,59 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+
+public class ValidateInputTest {
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"one", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        System.out.println(out);
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenMultipleRightInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int[] val = {input.askInt("Enter menu:"),
+                     input.askInt("Enter menu:")} ;
+
+        assertThat(val, is(new int[] {1,1}));
+    }
+
+    @Test
+    public void whenMinusInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"-1"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-1));
+    }
+
+
+}
