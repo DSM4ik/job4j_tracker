67d11dbbf9a6e2eba3f21f8f431a53004024fcea
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-18 06:39:51 +0300

    1. Паспорт и жители [#468355]

---
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. Паспорт и жители [#468355]
+ */
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        for (String p : citizens.keySet()) {
+            if (p.equals(passport)) {
+                return citizens.get(p);
+            }
+        }
+        return null;
+    }
+}
