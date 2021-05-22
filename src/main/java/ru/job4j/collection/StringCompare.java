3fe200dff0dbd5052b8e9491e2bf795192d8db91
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-22 20:12:19 +0300

    3. Компаратор для строк [#35008] - правка Math

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        int countIteration = o1.length() <= o2.length() ? o1.length() : o2.length();
+        int countIteration = Math.min(o1.length(), o2.length());
         for (int i = 0; i < countIteration; i++) {
             int rslt = Character.compare(o1.charAt(i), o2.charAt(i));
             if (rslt != 0) {
