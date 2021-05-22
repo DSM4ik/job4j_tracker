9d2d0267c52df7e9248621b6fb0fa7d30e38a60d
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-22 13:43:48 +0300

    0. Сортировка [#10096 #159350] - правка

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,6 +1,7 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class Item {
     private int id;
@@ -27,6 +28,23 @@ public class Item {
                 + ", name='" + name + '\''
                 + ", created=" + created
                 + "}";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     public int getId() {
--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -20,18 +20,32 @@ public class ItemTest {
                                          new Item(1, "Item1")
                                         );
         Collections.sort(items, new SortItemByAsc());
-        assertThat(items.get(0).getId(), is(1));
+        List<Item> itemsExpected = Arrays.asList(new Item(1, "Item1"),
+                                                new Item(2, "Item2"),
+                                                new Item(3, "item3"),
+                                                new Item(4, "item4"),
+                                                new Item(5, "item5")
+                                                );
+        assertThat(itemsExpected, is(items));
     }
 
     @Test
     public void sortByDesc() {
         List<Item> items = Arrays.asList(new Item(4, "item4"),
-                new Item(3, "item3"),
-                new Item(5, "item5"),
-                new Item(2, "Item2"),
-                new Item(1, "Item1")
+                                        new Item(3, "item3"),
+                                        new Item(5, "item5"),
+                                        new Item(2, "Item2"),
+                                        new Item(1, "Item1")
         );
+
+        List<Item> itemsExpected = Arrays.asList(new Item(5, "item5"),
+                                                new Item(4, "item4"),
+                                                new Item(3, "item3"),
+                                                new Item(2, "Item2"),
+                                                new Item(1, "Item1")
+        );
+
         Collections.sort(items, new SortItemByDesc());
-        assertThat(items.get(0).getId(), is(5));
+        assertThat(itemsExpected, is(items));
     }
 }
