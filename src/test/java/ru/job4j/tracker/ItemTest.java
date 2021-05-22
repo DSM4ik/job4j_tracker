3014117e4ad36b2682c935c65bb508c2531d7bf7
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-05-22 13:08:54 +0300

    0. Сортировка [#10096]

---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ItemTest {
+
+    @Test
+    public void sortByAsc() {
+        List<Item> items = Arrays.asList(new Item(4, "item4"),
+                                         new Item(3, "item3"),
+                                         new Item(5, "item5"),
+                                         new Item(2, "Item2"),
+                                         new Item(1, "Item1")
+                                        );
+        Collections.sort(items, new SortItemByAsc());
+        assertThat(items.get(0).getId(), is(1));
+    }
+
+    @Test
+    public void sortByDesc() {
+        List<Item> items = Arrays.asList(new Item(4, "item4"),
+                new Item(3, "item3"),
+                new Item(5, "item5"),
+                new Item(2, "Item2"),
+                new Item(1, "Item1")
+        );
+        Collections.sort(items, new SortItemByDesc());
+        assertThat(items.get(0).getId(), is(5));
+    }
+}
