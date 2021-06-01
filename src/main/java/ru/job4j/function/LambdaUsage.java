37cb0e02b58ab1049ae7d08c18425d199126377a
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-06-01 23:17:56 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,20 @@
+package ru.job4j.function;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 2.2. Лямбда блок [#249208]
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Petr", "Ivan", "Anton", "Valera");
+        Comparator<String> namesComp = (name1, name2) -> {
+            System.out.println("Element- " + name1 + " compare to Element -" + name2);
+            return name1.compareTo(name2);
+        };
+
+        names.sort(namesComp);
+    }
+}
