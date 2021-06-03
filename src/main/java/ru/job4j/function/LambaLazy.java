c302b5ac71e20969423e6741e0f846179ca7dd74
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-06-03 21:59:34 +0300

    3. Ссылки на методы [#70608 #163638] - правка

--- src/main/java/ru/job4j/function/LambaLazy.java
+++ src/main/java/ru/job4j/function/LambaLazy.java
@@ -5,12 +5,21 @@ import java.util.Comparator;
 
 public class LambaLazy {
     public static void main(String[] args) {
-        String[] names = {"Petr", "Petra"};
+        String[] names = {"Petr", "Petr2"};
         Comparator<String> lengthComp = (left, right) -> {
-            System.out.println("execute comparator");
-            return left.length() - right.length();
+            System.out.println("left length = " + left.length()
+                             + "; right length = " + right.length());
+            return Integer.compare(right.length(), left.length());
         };
 
+        for (String str : names) {
+            System.out.println(str);
+        }
+
         Arrays.sort(names, lengthComp);
+
+        for (String str : names) {
+            System.out.println(str);
+        }
     }
 }
