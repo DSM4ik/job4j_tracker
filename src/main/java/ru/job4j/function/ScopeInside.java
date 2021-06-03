8def2cae0ddbecee78e1ac56fd7dbbf5af9dba94
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-06-03 22:10:24 +0300

    4. Зона видимости в лямбда-выражениях и исключения [#70609 #163648] - правка

--- src/main/java/ru/job4j/function/ScopeInside.java
+++ src/main/java/ru/job4j/function/ScopeInside.java
@@ -3,15 +3,16 @@ package ru.job4j.function;
 import java.util.function.Supplier;
 
 /**
+ * 4. Зона видимости в лямбда-выражениях и исключения [#70609 #163648]
  * Переменную, объявленную вне лямбда-выражения, изменять внутри лямбда-выражения нельзя.
  * В лямбда-выражениях нельзя использовать методы, которые могут выбросить исключения.
  */
 public class ScopeInside {
+
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
         int total = 0;
-        for (int i = 0; i < number.length; i++) {
-            int num = number[i];
+        for (int num : number) {
             total = total + add(
                     () -> num
             );
