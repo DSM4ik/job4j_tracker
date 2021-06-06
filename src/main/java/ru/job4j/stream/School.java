0b89f654036a22a1a80a6eab9530746c3d0a7fe3
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-06-06 22:01:47 +0300

    1. Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/School.java
@@ -0,0 +1,14 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * 1. Фильтрация учеников [#110058]
+ */
+public class School {
+    public List<Student> collect(List<Student> students, Predicate<Student> predict) {
+        return students.stream().filter(predict).collect(Collectors.toList());
+    }
+}
