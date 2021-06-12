6501904766d4d0e228012cc3358bfc58d31312e0
Denis Mitskevich <den.mitskevich@hotmail.com>
2021-06-12 23:49:21 +0300

    0. Stream API улучшения [#58167]

---
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 0. Stream API улучшения [#58167]
+ */
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(st -> st != null)
+                .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
+                .takeWhile(st -> st.getScore() > bound)
+                .collect(Collectors.toList());
+    }
+}
