6537380d06084b2ad489e654c56d1d098c86bf9a
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-06-18 22:51:40 +0300

    1.6. Взаимодействие объектов [#166087 #126627]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -1,8 +1,8 @@
 package ru.job4j.oop;
 
 public class Jukebox {
-    public void music(int position){
-        if(position == 1) {
+    public void music(int position) {
+        if (position == 1) {
             System.out.println("Пусть бегут неуклюже");
         } else if (position == 2) {
             System.out.println("Спокойной ночи");
