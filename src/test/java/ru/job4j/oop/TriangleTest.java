595f1135e8a2a222ed5a49d15acbb067c0e52ff8
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-06-21 21:14:49 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #126635]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -17,4 +17,55 @@ public class TriangleTest {
         //assertThat(rsl, closeTo(8, 0.001));
         Assert.assertEquals(rsl, 8, 0.001);
     }
+
+    @Test
+    public void existTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
+        Assert.assertTrue(triangle.exist(ab, ac, bc));
+    }
+
+    @Test
+    public void semiperimeter682() {
+
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double ab = a.distance(b);
+        double ac = a.distance(c);
+        double bc = b.distance(c);
+        Assert.assertEquals(6.82, triangle.semiPerimeter(ab, ac, bc), 0.01);
+    }
+
+    @Test
+    public void areaTriangle799() {
+
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+
+        Triangle triangle = new Triangle(a, b, c);
+        System.out.println(triangle.area());
+        Assert.assertEquals(7.99, triangle.area(), 0.01);
+    }
+
+    @Test
+    public void areaTriangleNOminus1() {
+
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(0, 4);
+
+        Triangle triangle = new Triangle(a, b, c);
+        System.out.println(triangle.area());
+        Assert.assertEquals(-1, triangle.area(), 0.1);
+    }
 }
