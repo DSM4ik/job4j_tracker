95df01d9e402fd6616773754b26e3982099175cb
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-06-22 21:11:15 +0300

    5. Вызов переопределенного метода родителя - super.method ... [#504871]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -14,7 +14,6 @@ public class TriangleTest {
         Point c = new Point(0, 4);
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
-        //assertThat(rsl, closeTo(8, 0.001));
         Assert.assertEquals(rsl, 8, 0.001);
     }
 
@@ -33,7 +32,6 @@ public class TriangleTest {
 
     @Test
     public void semiperimeter682() {
-
         Point a = new Point(0, 0);
         Point b = new Point(4, 0);
         Point c = new Point(0, 4);
