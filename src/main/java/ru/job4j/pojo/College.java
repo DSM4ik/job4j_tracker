ab6e7bccbc4919b74638c7c0f6548638ceec55ae
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-06-23 22:02:38 +0300

    3. Массивы и модели. [#174892 #126649]

--- src/main/java/ru/job4j/pojo/College.java
+++ src/main/java/ru/job4j/pojo/College.java
@@ -5,10 +5,10 @@ import java.util.Date;
 public class College {
     public static void main(String[] args) {
         Student student = new Student();
-        student.setFIO("MickDS");
+        student.setFio("MickDS");
         student.setGroup(5);
         student.setDateIn(new Date());
 
-        System.out.println("студент " + student.getFIO() + " группа " + student.getGroup() + " Дата поступления " + student.getDateIn());
+        System.out.println("студент " + student.getFio() + " группа " + student.getGroup() + " Дата поступления " + student.getDateIn());
     }
 }
