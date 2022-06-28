6b14d4336958fee1703cdb203106516e148c02cc
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-06-28 21:42:39 +0300

    7. Шаблон проектирования - Стратегия [#786 #126598]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -32,14 +32,6 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        /*Item[] rsl = new Item[size];
-        int indexRsl = 0;
-        for (int i = 0; i < size ; i++) {
-            if (items[i] != null) {
-                rsl[indexRsl++] = items[i];
-            }
-        }
-        return Arrays.copyOf(rsl, indexRsl);*/
         return Arrays.copyOf(items, size);
     }
 
--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -17,4 +17,29 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         Assert.assertEquals(created.getName(), expected.getName());
     }
+
+    @Test
+    public void whenEditItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = {
+                String.valueOf(item.getId()), /* id сохраненной заявки в объект tracker. */
+                "edited item"
+        };
+        StartUI.editItem(new StubInput(answers), tracker);
+        Item edited = tracker.findById(item.getId());
+        Assert.assertEquals(edited.getName(), "edited item");
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("ItemNew");
+        tracker.add(item);
+        String[] answers = new String[] {"1"};
+        StartUI.deleteItem(new StubInput(answers), tracker);
+        Item deleted = tracker.findById(1);
+        Assert.assertEquals(deleted, null);
+    }
 }
